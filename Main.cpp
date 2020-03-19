212721b43bea15b5d9058c2e75381f69b9b30558
Camille <c.gagnonloiselle@hotmail.com>
2020-03-19 17:18:42 -0400

    ajout de la partie 2 du TP

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,74 @@
+// ConsoleApplication1.cpp : définit le point d'entrée pour l'application console.
 #include <iostream>
-int PGCD(int, int);
-void simplify(int*, int*);
+
+int readInt();
+int PGCD(int numerator, int denominator);
+void remplirTableau(int num[], int denom[], int);
+void affiche(int num[], int denom[], int);
+void simplify(int* numerator, int* denominator);
 
 int main() {
 
-	int numerator = 6;
-	int denominator = 12;
+	int nbFract;
 
-	int pgcd = PGCD(numerator, denominator);
-	std::cout << "Le PGCD est: " << pgcd << std::endl;
+	std::cout << "Saisir le nombre de fractions à analyser :" << std::endl;
+	std::cin >> nbFract;
 
-	simplify(&numerator, &denominator);
-	std::cout << "La Fraction simplifiee est: " << numerator << "/" << denominator << std::endl;
+	int* numTab = new int[nbFract];
+	int* denomTab = new int[nbFract];
+
+	remplirTableau(numTab, denomTab, nbFract);
+
+	affiche(numTab, denomTab, nbFract);
+}
+
+int readInt() {
+
+	int value;
+	bool valide = false;
+
+	while (!valide) {
+
+		std::cout << "Entrez la valeur: ";
+		std::cin >> value;
+
+		if (!std::cin.fail() && value > 0) {
+
+			valide = true;
+		}
+		else {
+
+			std::cout << "CE N'EST PAS UN INT VALIDE (DOIT ETRE PLUS GRAND QUE 0 ET ETRE UN NOMBRE ENTIER) !!!!" << std::endl;
+			std::cin.clear();
+			std::cin.ignore(999, '\n');
+			std::cout << std::endl;
+		}
+	}
+
+	return value;
+}
+
+void remplirTableau(int num[], int denom[], int nbFract) {
+
+	for (int i = 0; i < nbFract; i++) {
+		std::cout << "Saisir le numerateur de la fraction " << (i + 1) << " : " << std::endl;
+		num[i] = readInt();
+
+		std::cout << "Saisir le denominateur de la fraction " << (i + 1) << " : " << std::endl;
+		denom[i] = readInt();
+	}
+}
+
+void affiche(int num[], int denom[], int nbFract) {
+
+
+
+	for (int i = 0; i < nbFract; i++) {
+
+		std::cout << num[i] << "/" << denom[i] << " >>> ";
+		simplify(&num[i], &denom[i]);
+		std::cout << num[i] << "/" << denom[i] << std::endl;
+	}
 }
 
 int PGCD(int numerator, int denominator) {
@@ -33,15 +90,6 @@ int PGCD(int numerator, int denominator) {
 void simplify(int* numerator, int* denominator) {
 
 	int pgcd = PGCD(*numerator, *denominator);
-
-	if (numerator == denominator) {
-
-		*numerator = 1;
-		*denominator = 1;
-	}
-	else {
-
-		*numerator = (*numerator / pgcd);
-		*denominator = (*denominator / pgcd);
-	}
+	*numerator = (*numerator / pgcd);
+	*denominator = (*denominator / pgcd);
 }
